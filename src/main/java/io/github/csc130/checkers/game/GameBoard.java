931c607d43c7ba0dd3421011da26ae923ce50482
Xander Endre <45239432+ShadyAlexCodes@users.noreply.github.com>
2023-01-31 13:40:24 -0700

    depression p1

---
+++ src/main/java/io/github/csc130/checkers/game/GameBoard.java
@@ -0,0 +1,10 @@
+/**
+ * @author Xander
+ * @createdOn 1/31/2023 at 1:35 PM
+ * @projectName CasinoNight
+ * @packageName io.github.csc130.checkers.game;
+ */
+package io.github.csc130.checkers.game;
+
+public class GameBoard {
+}
---
+++ src/main/java/io/github/csc130/utils/CasinoUI.java
@@ -0,0 +1,10 @@
+/**
+ * @author Xander
+ * @createdOn 1/31/2023 at 1:34 PM
+ * @projectName CasinoNight
+ * @packageName io.github.csc130.utils;
+ */
+package io.github.csc130.utils;
+
+public class CasinoUI {
+}
