a7e34adfaba772f97545a4e8f9e418a75a6beeeb
RGonzalez03 <rgonzalez@student.neumont.edu>
2023-02-06 13:10:31 -0700

    idk what I did and idk if it works

--- src/main/java/io/github/csc130/GUI/Controllers/CheckerMe.java
+++ src/main/java/io/github/csc130/GUI/Controllers/CheckerMe.java
@@ -1,5 +1,38 @@
 package io.github.csc130.GUI.Controllers;
 
-public class CheckerMe {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CheckerMe implements Initializable {
+    @FXML
+    Button btnSlay;
+    @FXML
+    ComboBox gameModeDrop;
+    @FXML
+    ComboBox diffDrop;
+    @FXML
+    Button btnRTMM;
+    @FXML
+    Button btnQuit;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //difficulty settings
+        diffDrop.getItems().add("Easy");
+        diffDrop.getItems().add("Medium");
+        diffDrop.getItems().add("Harder daddy");
+
+        //game mode settings
+        gameModeDrop.getItems().add("Player vs Player");
+        gameModeDrop.getItems().add("Player vs CPU");
+        gameModeDrop.getItems().add("CPU vs CPU");
+
+
+
+    }
 }
