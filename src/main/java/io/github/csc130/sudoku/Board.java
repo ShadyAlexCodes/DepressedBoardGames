075fa2ef4e3660240eef632674eeb1af0105cc5c
EmileRandolph <98546063+EmileRandolph@users.noreply.github.com>
2023-02-06 14:14:43 -0700

    sudoku

---
+++ src/main/java/io/github/csc130/sudoku/Board.java
@@ -0,0 +1,27 @@
+package io.github.csc130.sudoku;
+
+public class Board {
+    protected int[][] board;
+    private int numberOfRows;
+    private int numberOfColumns;
+
+    public int getNumberOfRows() {
+        return numberOfRows;
+    }
+
+    public void setNumberOfRows(int numberOfRows) {
+        this.numberOfRows = numberOfRows;
+    }
+
+    public int getNumberOfColumns() {
+        return numberOfColumns;
+    }
+
+    public void setGetNumberOfColumns(int getNumberOfColumns) {
+        this.numberOfColumns = getNumberOfColumns;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+}
---
+++ src/main/java/io/github/csc130/sudoku/Difficulty.java
@@ -0,0 +1,5 @@
+package io.github.csc130.sudoku;
+
+public enum Difficulty {
+    EASY, MEDIUM, HARD
+}
